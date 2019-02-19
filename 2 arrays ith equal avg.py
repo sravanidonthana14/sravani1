c597e58062ba3d64a319e5975eae0a15cf8c6417
sravanidonthana14 <42735907+sravanidonthana14@users.noreply.github.com>
2019-02-19 10:36:04 +0530

    Update 2 arrays ith equal avg.py

--- 2 arrays ith equal avg.py
+++ 2 arrays ith equal avg.py
@@ -1,6 +1,26 @@
-g=int(input())
-a=[]
-a.append(input())
-if g>2 and g<100000:
-    print("yes")
-else:print("no")
+n=int(input())
+l=input().split()
+k=[]
+r=[]
+for x in l:
+    k.append(int(x))
+for x in range(0,len(k)):
+    r1=0
+    r2=0
+    c1=0
+    c2=0
+    for y in range(x+1,len(k)):
+        r1=r1+k[y]
+        c1=c1+1
+    if(c1>0):
+        r1=int(r1/(c1))
+    for z in range(0,x+1):
+        r2=r2+k[z] 
+        c2=c2+1
+    if(c2>0):
+        r2=int(r2/(c2))
+    if(r1==r2):
+        print("yes")
+        break
+else:
+    print("no")
