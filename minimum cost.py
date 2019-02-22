a466b34a193ff74f124ffbc07f257d8c2f632d6e
sravanidonthana14 <42735907+sravanidonthana14@users.noreply.github.com>
2019-02-22 12:07:03 +0530

    Create minimum cost.py

---
+++ minimum cost.py
@@ -0,0 +1,18 @@
+w,y=map(str,input().split(" "))
+lx=len(w)
+ly=len(y)
+c=0
+if ly>lw:
+    n=ly-lw
+    for i in range(n):
+        w+=" "
+elif lw>ly:
+    e=""
+    for i in range(ly):
+        e+=w[i]
+    w=e
+    c=c+(lw-ly)
+for i in range(len(w)):
+    if w[i]!=y[i]:
+        c+=1
+print(c)
